27d7d81a94f034f8186e2b3cd9cf9a027f4732eb
Susan <sjessopb89@gmail.com>
2020-09-08 13:34:15 -0400

    updates

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,64 +1,49 @@
-// Store our API endpoint inside queryUrl
+// Store API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson"
-console.log(queryUrl)
+  console.log(queryUrl)
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
+  // Once there is a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
 });
 
 function createFeatures(earthquakeData) {
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
+  function onEachLayer(feature) {
+    return new L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+      radius: circleSize(feature.properties.mag),
+      fillOpacity: 0.8,
+      color: getColor(feature.properties.mag),
+      fillColor: getColor(feature.properties.mag)
+    });
+  }
+
+  // Define a function to run once for each feature in the features array
+  // Give each feature a popup to describe the magnitude, place and time of the earthquake
   function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    layer.bindTooltip("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p><hr><p>" + feature.properties.mag + "</p>");
   }
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    pointToLayer: onEachLayer
   });
 
-  // Sending our earthquakes layer to the createMap function
+  // Send earthquakes layer to the createMap function
   createMap(earthquakes);
 }
-// // Loop through the cities array and create one marker for each city object
-// for (var i = 0; i < countries.length; i++) {
 
-//   // Conditionals for countries points
-//   var color = "";
-//   if (countries[i].points > 200) {
-//     color = "yellow";
-//   }
-//   else if (countries[i].points > 100) {
-//     color = "blue";
-//   }
-//   else if (countries[i].points > 90) {
-//     color = "green";
-//   }
-//   else {
-//     color = "red";
-//   }
-
-//   // Add circles to map
-//   L.circle(countries[i].location, {
-//     fillOpacity: 0.75,
-//     color: "white",
-//     fillColor: color,
-//     // Adjust radius
-//     radius: countries[i].points * 1500
-//   }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
-// }
+// Create maps
 function createMap(earthquakes) {
 
-  // Define streetmap and darkmap layers
+  // Define maps: streetmap, satellite and darkmap layers
   var streets =  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution:  "Map data &copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
    tileSize: 512,
-   maxZoom: 19,
+   maxZoom: 18,
    zoomOffset: -1,
    id: "mapbox/streets-v11",
    accessToken: API_KEY
@@ -91,7 +76,7 @@ function createMap(earthquakes) {
     Earthquakes: earthquakes
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // Create map and give the map and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
@@ -101,9 +86,36 @@ function createMap(earthquakes) {
   });
 
   // Create a layer control
-  // Pass in our baseMaps and overlayMaps
+  // Pass in the baseMaps and overlayMaps
   // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+}
+// Create function to set the color for different magnitude
+function getColor(magnitude) {
+  // Conditionals for magnitude
+  if (magnitude >= 5) {
+    return "darkred";
+  }
+  else if (magnitude >= 4) {
+    return "red";
+  }
+  else if (magnitude >= 3) {
+   return "darkorange";
+  }
+  else if (magnitude >= 2) {
+    return "yellow";
+  }
+  else if (magnitude >= 1) {
+    return "yellowgreen";
+  }
+  else {
+    return "green";
+  }
+};
+
+// Define a circleSize function that will give each size magnitude of the earthquake in each location
+function circleSize(magnitude) {
+return magnitude*3;
 }
